2d57a6b86986c59ed9ea21e869b723391ec537cf
ChristopherNS07 <paulchrisns@gmail.com>
2023-12-04 10:29:36 -0300

    Implementação da ClasseConexaoBD

---
+++ classeConexaoBD.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+class ConexaoBanco:
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self. password = password
+        self.database = database
+        self.conexao = None
+
+    def conectar(self):
+        try:
+            self.conexao = mysql.connector.connect(
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                database=self.database)
+
+            print("Conexão realizada com sucesso ")
+        except Exception as e:
+            print("Erro na conexão: ", e)
